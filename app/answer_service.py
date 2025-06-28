e80d09593a0b5dc4ecf0d5eedd13ff1f7e6a54af
gsmafra <gsenamafra@gmail.com>
2025-06-28 01:06:25 -0300

    create rectification queue

--- app/answer_service.py
+++ app/answer_service.py
@@ -1,12 +1,13 @@
 from app.db_access import (
+    add_to_rectification_queue,
     get_character,
     get_canonical_answer,
-    rectify_served_answer,
+    get_rectification_queue,
+    remove_from_rectification_queue,
     set_canonical_answer,
     set_served_answer,
     update_session,
     add_thumbs_down,
-    get_thumbs_down_answers,
     update_canonical_answer,
 )
 from app.gemini import get_gemini_answer
@@ -30,12 +31,13 @@ def get_or_generate_answer(db, device_id, question):
 
 def thumbs_down_answer(db, question, character, answer, device_id):
     add_thumbs_down(db, question, character, answer, device_id)
+    add_to_rectification_queue(db, character, question, answer)
 
 
 def get_answers_to_rectify(db):
-    return get_thumbs_down_answers(db)
+    return get_rectification_queue(db)
 
 
 def rectify_answer_service(db, character, question, original_answer, rectified_answer):
     update_canonical_answer(db, character, question, rectified_answer)
-    rectify_served_answer(db, character, question, original_answer)
+    remove_from_rectification_queue(db, character, question, original_answer)
