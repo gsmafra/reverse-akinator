b679e4517f0f8175b793fb144bc0b594d7d06ab1
gsmafra <gsenamafra@gmail.com>
2025-06-29 02:40:37 -0300

    use gemini 2.5

--- app/pipelines.py
+++ app/pipelines.py
@@ -1,7 +1,22 @@
 PIPELINES = {
-    "use_wikipedia": {"probability": 0.5, "use_wikipedia": True},
+    "use_wikipedia": {
+        "probability": 0.25,
+        "use_wikipedia": True,
+        "model": "gemini-2.0-flash",
+    },
+    "use_wikipedia_2.5": {
+        "probability": 0.25,
+        "use_wikipedia": True,
+        "model": "gemini-2.5-flash",
+    },
     "dont_use_wikipedia": {
-        "probability": 0.5,
+        "probability": 0.25,
         "use_wikipedia": False,
+        "model": "gemini-2.0-flash",
+    },
+    "dont_use_wikipedia_2.5": {
+        "probability": 0.25,
+        "use_wikipedia": False,
+        "model": "gemini-2.5-flash",
     },
 }
