e955098e4dc483777b9f978515dfcbc644e8d35a
gsmafra <gsenamafra@gmail.com>
2025-07-06 03:00:35 -0300

    ramp down gemini 2.0

--- app/llm/pipelines.py
+++ app/llm/pipelines.py
@@ -1,12 +1,12 @@
 PIPELINES = {
     "use_wikipedia": {
-        "probability": 0.25,
+        "probability": 0.1,
         "use_wikipedia": True,
         "model": "gemini-2.0-flash",
         "use_character_question_hint": False,
     },
     "use_wikipedia_2.5": {
-        "probability": 0.25,
+        "probability": 0.3,
         "use_wikipedia": True,
         "model": "gemini-2.5-flash",
         "use_character_question_hint": False,
@@ -18,13 +18,13 @@ PIPELINES = {
         "use_character_question_hint": False,
     },
     "dont_use_wikipedia_2.5": {
-        "probability": 0.25,
+        "probability": 0.3,
         "use_wikipedia": False,
         "model": "gemini-2.5-flash",
         "use_character_question_hint": False,
     },
     "nowiki_character_question_hint": {
-        "probability": 0.25,
+        "probability": 0.3,
         "use_wikipedia": False,
         "model": "gemini-2.5-flash",
         "use_character_question_hint": True,
